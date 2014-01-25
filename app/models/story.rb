1643191e48cc9df0f04dad855280eefa37e3d83c
Larahy <larahy@gmail.com>
2014-01-25 15:28:23 +0000

    Story 'hotness' implemented

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,6 +8,18 @@ class Story < ActiveRecord::Base
 
   def vote_count
     -votes.count
+  end 
+
+  def votes_per_minute       
+  	votes.count / ((Time.now - created_at) / 60)
+  end
+
+  def hotness
+  	if self.votes_per_minute > 100
+  		"Off the Richter"
+  	else 
+  		self.votes_per_minute.round(2)
+  	end 	
   end
 
 end
