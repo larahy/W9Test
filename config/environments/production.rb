e90ab43c6610d633623fbf5dab1082c66115662c
larahy <larahy@gmail.com>
2014-01-30 12:09:04 +0000

    Add Sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
